da88b049eb8e9fc1cb68b5b9d9aa3a11cbbc7dc8
Jack Franks <jonathan.franks@breaktech.com>
2026-05-08 17:58:47 -0400

    Add first set of tests

--- web/modules/custom/jackotopia/tests/src/ExistingSite/CoreStuffTest.php
+++ web/modules/custom/jackotopia/tests/src/ExistingSite/CoreStuffTest.php
@@ -1,10 +1,8 @@
 <?php
 
-namespace Drupal\Tests\jackotopia\CoreStuff;
+namespace Drupal\Tests\jackotopia\ExistingSite;
 
-use weitzman\DrupalTestTraits\ExistingSiteBase;
-
-class CoreStuffTest extends ExistingSiteBase {
+class CoreStuffTest extends JackotopiaTestBase {
 
   /**
    * Tests that a user can log in.
@@ -14,6 +12,37 @@ class CoreStuffTest extends ExistingSiteBase {
   public function testUserLogin() {
     $user = $this->createUser();
     $this->drupalLogin($user);
+  }
+
+  /**
+   * Tests that we can bypass the error checking and check the text on the page.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   * @throws \Behat\Mink\Exception\ResponseTextException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testErrorPage() {
+    $user = $this->createUser();
+    $this->drupalLogin($user);
+    $this->drupalGet('/jackotopia/error-message', allowErrors: TRUE);
+
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('It works!');
+    $this->assertSession()->pageTextContains('Uh oh, this looks like an error.');
+  }
+
+  /**
+   * Tests the errors page.
+   *
+   * We expect this test to fail as it sits because there is a Drupal error
+   * message displayed on this page.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testErrorAssertionsFail() {
+    $user = $this->createUser();
+    $this->drupalLogin($user);
+    $this->drupalGet('/jackotopia/error-message');
   }
 
 }
